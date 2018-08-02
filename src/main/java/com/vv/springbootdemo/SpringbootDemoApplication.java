d3c381181fcd220e0319001bb8dd419fdc420a30
imvv1993 <398841701@qq.com>
2018-08-02 17:40:24 +0800

    增加启动类注释

--- src/main/java/com/vv/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/vv/springbootdemo/SpringbootDemoApplication.java
@@ -3,6 +3,10 @@ package com.vv.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ */
 @SpringBootApplication
 public class SpringbootDemoApplication {
 

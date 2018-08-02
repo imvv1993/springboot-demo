49ee451855bedf655e491906cb9d79eeed86e107
imvv1993 <398841701@qq.com>
2018-08-02 17:26:33 +0800

    first commit

---
+++ src/main/java/com/vv/springbootdemo/controller/MyTest.java
@@ -0,0 +1,21 @@
+package com.vv.springbootdemo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MyTest {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "MyTest: Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MyTest.class, args);
+    }
+}
